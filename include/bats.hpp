adbac3a1911149152d4848e43e2d8a197bcd809a
Loic planchon <loic.planchon@epitech.eu>
2023-02-12 13:59:27 +0100

    [ADD] vecter de bats crée a part (trying to make it work now)

---
+++ include/bats.hpp
@@ -0,0 +1,22 @@
+
+#ifndef BATS_HPP_
+#define BATS_HPP_
+
+#include <memory>
+#include <vector>
+#include "Sprite.hpp"
+#include "bat.hpp"
+
+class Bats {
+    public:
+        Bats();
+        ~Bats();
+
+        void Batss();
+
+    protected:
+    private:
+        Bat _bat;
+};
+
+#endif /* !BATS_HPP_ */
---
+++ src/bats.cpp
@@ -0,0 +1,10 @@
+
+#include "../include/bats.hpp" // inclure la classe Bat
+
+void Bats::Batss() {
+    std::vector<Bat> bats; // créer un vecteur de Bat
+
+    bats.push_back(Bat());
+    bats.push_back(Bat());
+    bats.push_back(Bat());
+}
